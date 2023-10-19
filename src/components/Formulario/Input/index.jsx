6b7f780b121db89ece6d879ceb872791615ea220
Gustavo <duarte.guga2025@gmail.com>
2023-10-18 22:08:27 -0400

    Reutilizando o input aprendendo

--- src/components/Formulario/Input/index.jsx
+++ src/components/Formulario/Input/index.jsx
@@ -1,9 +1,12 @@
 import styles from "./Styles.module.css"
 
-export default function Input({ type, placeholder}) {
+export default function Input({ type, text, name, placeholder, handleOnChange, value }) {
     return (
-        <div className={styles.input}>
-            <input type={type} placeholder={placeholder} />
+        <div>
+            <label htmlFor={name}>{text}:</label>
+            <input type={type} name={name} id={name} placeholder={placeholder} onChange={handleOnChange} value={value} />
         </div>
     )
 }
+
+<input type="text" className={styles.inputbordas} placeholder="Titulo" required="" value={evento.titulo} onChange={e => setEvento({ ...evento, titulo: e.target.value })} />
--- src/pages/test/index.jsx
+++ src/pages/test/index.jsx
@@ -1,11 +1,20 @@
-import Input from "@/components/Formulario/Input";
+import axios from "axios"
+import Input from "@/components/Formulario/Input"
+import { useState } from "react"
 
-export default function Home() {
+export default function Form() {
     return (
+        
+
+
         <>
-            <Input
-            type={"number"}
-            placeholder={"ola"} />
+            <form>
+
+                <Input type="text" text="Nome do projeto" name=""/>
+
+                <button > Enviar</button>
+            </form>
         </>
     )
 }
+
